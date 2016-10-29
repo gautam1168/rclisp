1b957160da398a3d82bd062af10226fff2f824bd
gautam1168 <gautam1168@gmail.com>
2016-10-29 23:19:38 +0530

    Adding remaining changes.

--- src/compileFSM.c
+++ src/compileFSM.c
@@ -3,6 +3,6 @@
 int main(int argc, char ** argv){
   FSM machine = compile_FSM("annie");
   print_FSM(machine);
-  printf("Testing annie: %d\n", FSM_test(machine, "dsafannied31rfa"));
+  printf("Testing dsafannied31rfa: %d\n", FSM_test(machine, "dsafannied31rfa"));
   return 0;
 }
